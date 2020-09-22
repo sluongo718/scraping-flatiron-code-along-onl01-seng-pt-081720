beaf89f2c11d4682071cbb809849e1d4b2a1582f
sam l <sluongo718@yahoo.com>
2020-09-22 19:36:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -17,7 +18,7 @@ class Scraper
   end
   
   def get_page
-    
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
 end
